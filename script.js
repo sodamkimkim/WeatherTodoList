6670aea59c5e4aae9d43a7fefc1e93dca2c52cc3
sodamkimkim <theka265@gmail.com>
2024-02-05 21:56:12 +0900

    appendChild

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const KeyCodeCheck = function () {
-    // console.log(window.event);
-    if (window.event.keyCode === 13)
-    {
-        const inputvalue = document.querySelector("#todo-input");
-        console.log(inputvalue.value);
-        inputvalue.value = "";
-    }  
+  // console.log(window.event);
+  if (window.event.keyCode === 13) {
+    const todoInput = document.querySelector("#todo-input");
+    const todoList = document.querySelector("#todo-list");
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newSpan);
+    console.log(newLi);
+      todoList.appendChild(newLi);
+      todoInput.value= "";
+  }
 };
