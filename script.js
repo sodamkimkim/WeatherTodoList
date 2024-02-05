df49258c7372fba38cbad2d03c9a76732e3d0b01
sodamkimkim <theka265@gmail.com>
2024-02-05 21:57:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+const todoInput = document.querySelector("#todo-input");
 const KeyCodeCheck = function () {
   // console.log(window.event);
-  if (window.event.keyCode === 13) {
-    const todoInput = document.querySelector("#todo-input");
+  if (window.event.keyCode === 13 && todoInput.value !=="") {
     const todoList = document.querySelector("#todo-list");
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
     newSpan.textContent = todoInput.value;
     newLi.appendChild(newSpan);
     console.log(newLi);
-      todoList.appendChild(newLi);
-      todoInput.value= "";
+    todoList.appendChild(newLi);
+    todoInput.value = "";
   }
 };
