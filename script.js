bdc1d65389faee6c4cbfe41befcdcb42b3b3474a
sodamkimkim <theka265@gmail.com>
2024-02-09 13:39:56 +0900

    btn addEventListner와 class toggle

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const todoInput = document.querySelector("#todo-input");
-const KeyCodeCheck = function () {
+
+const createToto = function () {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+  const vSpan = document.createElement("span");
+  vSpan.innerText = "V";
+
+
+
+  newBtn.addEventListener('click', () => {
+    newLi.classList.toggle('complete'); // 클릭할 때 마다 compelete class 붙었다 뗐다 됨.
+    if (newBtn.contains(vSpan))
+      newBtn.removeChild(vSpan);
+    else
+      newBtn.appendChild(vSpan);
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+}
+const keyCodeCheck = function () {
   // console.log(window.event);
-  if (window.event.keyCode === 13 && todoInput.value !=="") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-    newSpan.textContent = todoInput.value;
-    newLi.appendChild(newSpan);
-    console.log(newLi);
-    todoList.appendChild(newLi);
-    todoInput.value = "";
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createToto();
   }
 };
